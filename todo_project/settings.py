1c7f885461ad109c0ec9f3808f3e4ae8decebfdf
DanielH <danielh@nimble.la>
2022-11-21 12:23:24 -0500

    test

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -124,7 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_TEMP = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
+
+os.makedirs(STATIC_TEMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)  
-django_heroku.settings(locals())                        
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
